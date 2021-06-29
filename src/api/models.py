fee37add153d49469d28b126bc3c025d02372ebd
Gormm1980 <guillermosantamariarodrigo@hotmail.com>
2021-06-29 16:34:34 +0000

    delete class event

--- src/api/models.py
+++ src/api/models.py
@@ -49,12 +49,12 @@ class User(db.Model, BasicMode):
         }
 
 
-#  Fathers class
+#  class
 
 class Glace(db.Model, BasicMode):
-    __table__ = "Glazed"
+    __table__ = "Glaced"
     id_glace = db.Column(db.Integer,unique = True, primary_key= True)
-    image = db.Column(db.integer)
+    image = db.Column(db.Integer)
     name = db.Column(db.String(80), unique = True)
     description = db.Column(db.String(250), nullable=False)
     price = db.Column(db.Integer(20), nullable=False)
@@ -113,7 +113,7 @@ class TreatYou(db.Model, BasicMode):
 class Cakes(db.Model, BasicMode):
     __table__ = "Cakes"
     id_cakes = db.Column(db.Integer,unique = True, primary_key= True)
-    image = db.Column(db.integer)
+    image = db.Column(db.Integer)
     name = db.Column(db.String(80), unique = True)
     description = db.Column(db.String(250), nullable=False)
     price = db.Column(db.Integer(20), nullable=False)
@@ -143,7 +143,7 @@ class Cakes(db.Model, BasicMode):
 class SpreadLove(db.Model, BasicMode):
     __table__ = "SpreadLove"
     id_spread = db.Column(db.Integer,unique = True, primary_key= True)
-    image = db.Column(db.integer)
+    image = db.Column(db.Integer)
     name = db.Column(db.String(80), unique = True)
     description = db.Column(db.String(250), nullable=False)
     price = db.Column(db.Integer(20), nullable=False)
@@ -169,104 +169,6 @@ class SpreadLove(db.Model, BasicMode):
             "info_price": self.info_price
             # do not serialize the password, its a security breach
         }
-class Events(db.Model, BasicMode):
-    __table__ = "Events"
-    id_events = db.Column(db.Integer,unique = True, primary_key= True)
-    image = db.Column(db.integer)
-    name = db.Column(db.String(80), unique = True)
-    description = db.Column(db.String(250), nullable=False)
-    price = db.Column(db.Integer(20), nullable=False)
-    info_price = db.Column(db.String(250), nullable=False)
 
-    def db_post(self):        
-        db.session.add(self)
-        db.session.commit()
 
-    def set_with_event(self,json):
-        self.name = json["name"]
-        self.description = json["description"]
-        self.price = json["price"]
-        self.info.price = json["info.price"]
-        return self
-
-    def serialize(self):
-        return {
-            "id_events": self.id_events,
-            "image": self.image,
-            "description": self.description,
-            "price": self.price,
-            "info_price": self.info_price
-            # do not serialize the password, its a security breach
-        }
-
-    # Children per category
-# class Variety_Glacez(db.Model, BasicMode):
-
-#     id_variety = db.Column(db.Integer,unique = True, primary_key= True)
-#     name = db.Column(db.String(80), unique = True)
-#     description = db.Column(db.String(250), nullable=False)
-#     price = db.Column(db.Integer(20), nullable=False)
-#     info_price = db.Column(db.String(250), nullable=False)
-#     glace_id = db.Column(db.Integer, db.ForeignKey("Glace.id_glace"))
-
-#     def serialize(self):
-#         return {
-#             "id_events": self.id_events,
-#             "description": self.description,
-#             "price": self.price,
-#             "info_price": self.info_price
-#             # do not serialize the password, its a security breach
-#         }
-
-# class Variety_Cakes(db.Model, BasicMode):
-
-#     id_varietyCake = db.Column(db.Integer,unique = True, primary_key= True)
-#     name = db.Column(db.String(80), unique = True)
-#     description = db.Column(db.String(250), nullable=False)
-#     price = db.Column(db.Integer(20), nullable=False)
-#     info_price = db.Column(db.String(250), nullable=False)
-#     cakes_id = db.Column(db.Integer, db.ForeignKey("Cakes.id_cakes"))
-#     def serialize(self):
-#         return {
-#             "id_varietycake": self.id_varietyCake,
-#             "description": self.description,
-#             "price": self.price,
-#             "info_price": self.info_price
-#             # do not serialize the password, its a security breach
-#         }
-
-# class Variety_Spread(db.Model, BasicMode):
-
-#     id_varietyspread = db.Column(db.Integer,unique = True, primary_key= True)
-#     name = db.Column(db.String(80), unique = True)
-#     description = db.Column(db.String(250), nullable=False)
-#     price = db.Column(db.Integer(20), nullable=False)
-#     info_price = db.Column(db.String(250), nullable=False)
-#     spread_id = db.Column(db.Integer, db.ForeignKey("SpreadLove.id_spread"))
-
-#     def serialize(self):
-#         return {
-#             "id_events": self.id_events,
-#             "description": self.description,
-#             "price": self.price,
-#             "info_price": self.info_price
-#             # do not serialize the password, its a security breach
-#         }
-
-# class Pack_Events(db.Model, BasicMode):
-
-#     id_packEvents = db.Column(db.Integer,unique = True, primary_key= True)
-#     name = db.Column(db.String(80), unique = True)
-#     description = db.Column(db.String(250), nullable=False)
-#     price = db.Column(db.Integer(20), nullable=False)
-#     info_price = db.Column(db.String(250), nullable=False)
-#     events_id = db.Column(db.Integer, db.ForeignKey("Events.id_events"))
-
-#     def serialize(self):
-#         return {
-#             "id_packEvents": self.id_packEvents,
-#             "description": self.description,
-#             "price": self.price,
-#             "info_price": self.info_price
-#             # do not serialize the password, its a security breach
-#         }
+    
