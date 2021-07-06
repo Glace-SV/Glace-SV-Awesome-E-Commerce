ae0b2d1a14a2c65336cab0c6a3e72675b914d1d1
Diana Ware <dianasofia.ware@gmail.com>
2021-07-06 17:21:41 +0000

    final login/register page

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -29,13 +29,13 @@ export const Login = props => {
 	const params = useParams();
 
 	return (
-		<div className="bckgrndimg">
+		<div className="loginbckgrndimg">
 			<Tab.Container id="left-tabs-example" defaultActiveKey="first">
 				<div className="row rowdesign1">
 					<Nav variant="pills" className="mt-3 mb-3 mx-auto">
 						<Nav.Item className="col-lg-6 col-6 mx-auto text-center">
 							<Nav.Link eventKey="first" variant="warning">
-								Login
+								Accede
 							</Nav.Link>
 						</Nav.Item>
 						<Nav.Item className="col-lg-6 col-6 mx-auto text-center">
@@ -45,8 +45,45 @@ export const Login = props => {
 				</div>
 				<div className="row rowdesign2">
 					<Tab.Content className="col-lg-6 col-12 mx-auto">
-						<Tab.Pane eventKey="first">hello</Tab.Pane>
-						<Tab.Pane eventKey="second">again</Tab.Pane>
+						<Tab.Pane eventKey="first">
+							<Form className="formcolor">
+								<Form.Group className="mb-3" controlId="formBasicEmail">
+									<Form.Label>Email</Form.Label>
+									<Form.Control type="email" placeholder="Email" required />
+								</Form.Group>
+
+								<Form.Group className="mb-3" controlId="formBasicPassword">
+									<Form.Label>Contraseña</Form.Label>
+									<Form.Control type="password" placeholder="Contraseña" required />
+								</Form.Group>
+								<Button variant="warning" type="submit">
+									Acceder
+								</Button>
+							</Form>
+						</Tab.Pane>
+						<Tab.Pane eventKey="second">
+							<Form className="formcolor">
+								<Form.Group className="mb-3" controlId="formBasicEmail">
+									<Form.Label>Nombre</Form.Label>
+									<Form.Control type="text" placeholder="Nombre" required />
+								</Form.Group>
+
+								<Form.Group className="mb-3" controlId="formBasicPassword">
+									<Form.Label>Email</Form.Label>
+									<Form.Control type="email" placeholder="Email" required />
+									<Form.Text>
+										Se enviará una contraseña a tu dirección de correo electrónico.
+									</Form.Text>
+									<Form.Text>
+										Tus datos personales se utilizarán para procesar tu pedido, mejorar tu
+										experiencia en esta web, gestionar el acceso a tu cuenta y otros propósitos.
+									</Form.Text>
+								</Form.Group>
+								<Button variant="warning" type="submit">
+									Registrarme
+								</Button>
+							</Form>
+						</Tab.Pane>
 					</Tab.Content>
 				</div>
 			</Tab.Container>
