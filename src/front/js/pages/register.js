fa25d7ff9f7b1ec0562abc58f649b84c8c3d9cf4
Gormm1980 <guillermosantamariarodrigo@hotmail.com>
2021-07-19 16:18:28 +0000

    modify styles in register.js

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -28,67 +28,53 @@ const Register = () => {
 	return (
 		<div className="container-fluid">
 			<div className="row rowdesign2">
-				<Row>
-					<Col>
-						<form>
-							<label htmlFor="name"> Nombre:</label>
-							<input type="text" id="name" name="name" />
-						</form>
-					</Col>
-					<Col>
-						<form>
-							<label htmlFor="last_name">Apellido:</label>
-							<input type="text" id="last_name" name="last_name" />
-						</form>
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<form>
-							<label htmlFor="adress">Dirección:</label>
-							<input type="text" id="adress" name="adress" />
-						</form>
-					</Col>
-					<Col>
-						<form>
-							<label htmlFor="city">Ciudad:</label>
-							<input type="text" id="city" name="city" />
-						</form>
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<Col>
-							<form>
-								<label htmlFor="Phone">Teléfono:</label>
-								<input type="text" id="phone" name="phone" />
-							</form>
-						</Col>
-						<form>
-							<label htmlFor="username"> Usuario:</label>
-							<input type="text" id="username" name="username" />
-						</form>
-					</Col>
-				</Row>
-				<row>
-					<Col>
-						<form>
-							<label htmlFor="password">Contraseña:</label>
-							<input type="text" id="password" name="password" />
-						</form>
-					</Col>
-					<Col>
-						<form>
-							<label htmlFor="email">Email:</label>
-							<input type="text" id="email" name="email" />
-						</form>
-					</Col>
-				</row>
+				<Tab.Content className="col-lg-6 col-12 mx-auto">
+					<Form className="formcolor">
+						<Form.Group className="mb-3" controlId="formBasicEmail">
+							<Form.Label>Email</Form.Label>
+							<Form.Control type="email" placeholder="Email" required />
+						</Form.Group>
+
+						<Form.Group className="mb-3" controlId="formBasicPassword">
+							<Form.Label>Contraseña</Form.Label>
+							<Form.Control type="password" placeholder="Contraseña" required />
+						</Form.Group>
+						<Form.Group className="mb-3" controlId="formBasicEmail">
+							<Form.Label>Username</Form.Label>
+							<Form.Control type="username" placeholder="Username" required />
+						</Form.Group>
+
+						<Form.Group className="mb-3" controlId="formBasicPassword">
+							<Form.Label>Nombre</Form.Label>
+							<Form.Control type="name" placeholder="Nombre" required />
+						</Form.Group>
+						<Form.Group className="mb-3" controlId="formBasicEmail">
+							<Form.Label>Apellidos</Form.Label>
+							<Form.Control type="last_name" placeholder="Apellido" required />
+						</Form.Group>
+
+						<Form.Group className="mb-3" controlId="formBasicPassword">
+							<Form.Label>Dirección</Form.Label>
+							<Form.Control type="adress" placeholder="Dirección" required />
+						</Form.Group>
+						<Form.Group className="mb-3" controlId="formBasicEmail">
+							<Form.Label>Ciudad</Form.Label>
+							<Form.Control type="city" placeholder="Ciudad" required />
+						</Form.Group>
+
+						<Form.Group className="mb-3" controlId="formBasicPassword">
+							<Form.Label>Teléfono</Form.Label>
+							<Form.Control type="phone" placeholder="Teléfono" required />
+						</Form.Group>
+						<Button variant="warning" type="submit">
+							Acceder
+						</Button>
+					</Form>
+				</Tab.Content>
 				<p>
 					Tus datos personales se utilizarán para procesar tu pedido, mejorar tu experiencia en esta web,
 					gestionar el acceso a tu cuenta y otros propósitos.
 				</p>
-				<Button variant="warning">Registrate</Button>{" "}
 			</div>
 		</div>
 	);
