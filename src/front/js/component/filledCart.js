50baff9c49e78455df9962727f553fcafc8edd32
Moskzow <inmoskwetrust@gmail.com>
2021-07-26 16:46:00 +0000

    Paypal button is working

--- src/front/js/component/filledCart.js
+++ src/front/js/component/filledCart.js
@@ -4,7 +4,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import PaypalCheckoutButton from "./paypalCheckoutButton";
 
-const FilledCart = () => {
+export const FilledCart = () => {
 	const { store, actions } = useContext(Context);
 	useEffect(() => {
 		actions.getUser();
@@ -13,105 +13,101 @@ const FilledCart = () => {
 	const user = store.currentUser;
 	console.log(user);
 	console.log(cart);
-	function OpenOrder() {
-		const order = {
-			customer: "123456",
-			total: "550.00",
-			items: [
-				{
-					sku: "112",
-					name: "Camisa ReactJS",
-					price: "300.00",
-					quantity: 1,
-					currency: "EUR"
-				},
-				{
-					sku: "99",
-					name: "Camisa JS",
-					price: "125.00",
-					quantity: 2,
-					currency: "EUR"
-				}
-			]
-		};
+	const order = {
+		customer: "Nombre",
+		total: "550.00",
+		items: [
+			{
+				sku: "112",
+				name: "Camisa ReactJS",
+				price: "300.00",
+				quantity: 1,
+				currency: "EUR"
+			},
+			{
+				sku: "99",
+				name: "Camisa JS",
+				price: "125.00",
+				quantity: 2,
+				currency: "EUR"
+			}
+		]
+	};
 
-		return (
-			<div>
-				{cart.map((item, index) => (
-					<div key={index} className="row d-inline-block mx-4">
-						<Card style={{ width: "17rem" }} className="col-12 m-3">
-							<Card.Img
-								variant="top"
-								className="imgsizingcategory categoryline"
-								src="https://unsplash.com/photos/5K5Nc3AGF1w/download?force=true&w=2400"
-							/>
-							<Card.Body>
-								<Card.Title>{item.name}</Card.Title>
-								<Card.Text>{item.description}</Card.Text>
-								<Card.Text>{item.size}</Card.Text>
-								<Card.Text>${parseInt(item.price)}</Card.Text>
-								<div className="row mx-auto">
-									<Button
-										className="mt-4"
-										variant="warning"
-										onClick={() => {
-											actions.deleteFromCart(index);
-										}}>
-										Eliminar
-									</Button>
-									<div className="col-5 text-center mt-1">{item.quantity}</div>
-									<Button
-										className="subs col-2"
-										variant="warning"
-										onClick={() => {
-											actions.sumCartItem(index, item.quantity, item.price);
-										}}>
-										+
-									</Button>
-								</div>
+	return (
+		<div>
+			{cart.map((item, index) => (
+				<div key={index} className="row d-inline-block mx-4">
+					<Card style={{ width: "17rem" }} className="col-12 m-3">
+						<Card.Img
+							variant="top"
+							className="imgsizingcategory categoryline"
+							src="https://unsplash.com/photos/5K5Nc3AGF1w/download?force=true&w=2400"
+						/>
+						<Card.Body>
+							<Card.Title>{item.name}</Card.Title>
+							<Card.Text>{item.description}</Card.Text>
+							<Card.Text>{item.size}</Card.Text>
+							<Card.Text>${parseInt(item.price)}</Card.Text>
+							<div className="row mx-auto">
+								<Button
+									className="add col-2"
+									variant="warning"
+									onClick={() => {
+										actions.subsCartItem(index, item.quantity);
+									}}>
+									-
+								</Button>
+								<div className="col-5 text-center mt-1">{item.quantity}</div>
+								<Button
+									className="subs col-2"
+									variant="warning"
+									onClick={() => {
+										actions.sumCartItem(index, item.quantity, item.price);
+									}}>
+									+
+								</Button>
+							</div>
+							<Button
+								className="mt-4"
+								variant="warning"
+								onClick={() => {
+									actions.deleteFromCart(index);
+								}}>
+								Eliminar
+							</Button>
+							<div>
 								<Button
 									className="mt-4"
 									variant="warning"
 									onClick={() => {
-										actions.deleteFromCart(index);
+										actions.updateItemPrice(index, item.quantity, item.price);
 									}}>
-									Eliminar
+									Actualizar total
 								</Button>
-								<div>
-									<Button
-										className="mt-4"
-										variant="warning"
-										onClick={() => {
-											actions.updateItemPrice(index, item.quantity, item.price);
-										}}>
-										Actualizar total
-									</Button>
-								</div>
-							</Card.Body>
-						</Card>
-					</div>
-				))}
-				;
-				<div className="mx-auto" id="categories">
-					<div className="row mb-4">
-						<h1 className="mt-3 mx-auto viewstitle">Verifica el total de tu orden</h1>
-					</div>
-					<h3 className="text-center">Gran total: $</h3>
+							</div>
+						</Card.Body>
+					</Card>
 				</div>
-				<div className="mx-auto" id="categories">
-					<div className="row mb-4">
-						<h1 className="mt-3 mx-auto viewstitle">Verifica los datos de tu orden</h1>
-					</div>
-					<h3 className="text-center">Gran total: $</h3>
+			))}
+			<div className="mx-auto" id="categories">
+				<div className="row mb-4">
+					<h1 className="mt-3 mx-auto viewstitle">Verifica el total de tu orden</h1>
 				</div>
-				<>
-					<header className="App-header">
-						<PaypalCheckoutButton order={order} />
-					</header>
-				</>
+				<h3 className="text-center">Gran total: $</h3>
 			</div>
-		);
-	}
+			<div className="mx-auto" id="categories">
+				<div className="row mb-4">
+					<h1 className="mt-3 mx-auto viewstitle">Verifica los datos de tu orden</h1>
+				</div>
+				<h3 className="text-center">Nombre: {user.name}</h3>
+				<h3 className="text-center">Dirección: {user.adress}</h3>
+			</div>
+			<>
+				<header className="App-header">
+					<PaypalCheckoutButton order={order} />
+				</header>
+			</>
+		</div>
+	);
 };
-
-export default FilledCart;
