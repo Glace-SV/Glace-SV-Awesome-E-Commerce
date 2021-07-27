bd87a26f1d898b0749b38b01f45e06888e7cf4c7
Moskzow <inmoskwetrust@gmail.com>
2021-07-27 17:24:33 +0000

    Add padding bottom betwen paypal button and footer

--- src/front/js/component/filledCart.js
+++ src/front/js/component/filledCart.js
@@ -112,7 +112,7 @@ export const FilledCart = () => {
 				</div>
 			</div>
 			<>
-				<header className="Paypal-header text-center">
+				<header className="Paypal-header text-center my-5">
 					<PaypalCheckoutButton order={order} />
 				</header>
 			</>
